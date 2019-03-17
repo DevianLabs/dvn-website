d4bad3f32ac9bcb853692401738ac296c1ba3700
Mig Salazar <mig.salazars@gmail.com>
2019-03-17 01:31:54 -0600

    save

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import './plugins/vuex';
 import { i18n } from './plugins/vue-i18n';
 import { router } from './plugins/vue-router';
 import './plugins/vuex-router-sync';
-import './plugins/register-service-worker';
 import './assets/styles/main.scss';
 import App from './App.vue';
 import store from './store';
